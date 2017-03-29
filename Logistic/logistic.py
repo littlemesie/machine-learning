faa5b89c7feecbe347276968ac1fcbfc5f54b218
littlemesie <littlemesie@163.com>
2017-03-29 23:42:00 +0800

    添加贝叶斯算法

--- Logistic/logistic.py
+++ Logistic/logistic.py
@@ -4,7 +4,11 @@
     logistic算法
 '''
 
+import sys
 from numpy import *
+import matplotlib.pyplot as plt
+
+# sys.setrecursionlimit(10000)
 
 # 打开数据
 def loadDataSet():
@@ -26,13 +30,45 @@ def gradAscent(dataMatIn, classLabels):
     labelMat = mat(classLabels).transpose() #convert to NumPy matrix
     m,n = shape(dataMatrix)
     alpha = 0.001
-    maxCycles = 500
+    maxCycles = 500 # 迭代次数
     weights = ones((n,1))
     for k in range(maxCycles):              #heavy on matrix operations
+        # 矩阵相乘
         h = sigmoid(dataMatrix*weights)     #matrix mult
         error = (labelMat - h)              #vector subtraction
-        weights = weights + alpha * dataMatrix.transpose()* error #matrix mult
+        weights = weights + alpha * dataMatrix.transpose()* error # 梯度上升法计算
     return weights
 
+# 画图
+def plotBestFit(w):
+    weights = w.getA()
+    # print w
+    dataMat,labelMat=loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i])== 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    # 拟合一条最佳直线
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1'); plt.ylabel('X2');
+    plt.show()
+
+
 data,lables = loadDataSet()
-print data
+# print data
+
+w = gradAscent(data,lables)
+
+plotBestFit(w)
+#print w
---
+++ NaiveBayes/bayes.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+'''
+    朴素贝叶斯分类器
+'''
+from numpy import *
+
+def loadDataSet():
+    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0,1,0,1,0,1]
+    return postingList,classVec
