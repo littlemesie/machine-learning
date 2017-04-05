d954423ec167d494a201233a79adc1314d1b107c
littlemesie <littlemesie@163.com>
2017-04-05 22:00:04 +0800

    朴素贝叶斯算法

--- NaiveBayes/bayes.py
+++ NaiveBayes/bayes.py
@@ -3,6 +3,7 @@
     朴素贝叶斯分类器
 '''
 from numpy import *
+import re
 
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
@@ -69,6 +70,14 @@ def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     else:
         return 0
 
+# 词袋模型 vocabList词向量 inputSet输入文档
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+    return returnVec
+
 def testingNB():
     listOPosts,listClasses = loadDataSet()
     myVocabList = createVocabList(listOPosts)
@@ -83,16 +92,66 @@ def testingNB():
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
     print testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb)
 
-testingNB()
+# testingNB()
+
+
+# 分词
+def textParse(bigString):
+    listOfTokens = re.split(r'\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+# 测试函数
+def spamTest():
+    docList = [];
+    classList = [];
+    fullText = []
+    for i in range(1, 26):
+        # 导入文件 并解析成词列表
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)  # create vocabulary
+    trainingSet = range(50);
+    testSet = []  # 随机构建测试函数
+    for i in range(10):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del (trainingSet[randIndex])
+    trainMat = [];
+    trainClasses = []
+    for docIndex in trainingSet:  # 测试分类器
+        trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:  # classify the remaining items
+        wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+            print "classification error", docList[docIndex]
+    e = float(errorCount) / len(testSet)
+    return e
+    print 'the error rate is: ', e
 
 
 
+# error =  0
+# for i in range(100):
+#     error += spamTest()
+# error = error / 100
+# print error
 postingList,classVec = loadDataSet()
 vocabSet = createVocabList(postingList)
-vec = setOfWords2Vec(vocabSet,postingList[0])
+# vec = setOfWords2Vec(vocabSet,postingList[0])
+vec = bagOfWords2VecMN(vocabSet,['love', 'my', 'dalmation'])
 trainMat = train_matrix(postingList,vocabSet)
 p0,p1,pA = trainNB0(trainMat,classVec)
 # print vocabSet[26]
 # print p0
-# print p1
+# print vec
 
