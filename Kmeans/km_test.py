0ea5c6b01f3901b46ed440b487f9d5f67a45be1c
littlemesie <ranjw@akulaku.com>
2019-01-16 20:58:14 +0800

    update

---
+++ Kmeans/km_test.py
@@ -0,0 +1,9 @@
+
+def loadDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float,curLine)
+        dataMat.append(fltLine)
+    return dataMat
